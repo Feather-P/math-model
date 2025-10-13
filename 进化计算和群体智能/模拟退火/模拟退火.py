49dffcaa419f4d157da73a67cd443c3cae3cf0fa
Feather-P <featherp@qq.com>
2025-10-13 08:57:15 +0800

    Add 模拟退火

---
+++ 进化计算和群体智能/模拟退火/模拟退火.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+def aimFunction(x):
+    y = x ** 3 - 60 * x ** 2 - 4 * x + 6
+    return y
+
+x = [i / 10 for i in range(1000)]
+y = [0.0 for i in range(1000)]
+for i in range(1000):
+    y[i] = aimFunction(x[i])
+
+plt.plot(x, y)
+plt.show()
+
+T = 1000 # initiate temperature
+Tmin = 10 # minimum value of temperature
+x = np.random.uniform(low=0, high=100) # initiate x
+k = 50 # times of internal circulation
+
+y = 0 # initiate result
+t = 0 # time
+while T >= Tmin:
+    for i in range(k):
+        # calculate y
+        y = aimFunction(x)
+        # generate a new x in the neighboorhood of x by transform function
+        xNew = x + np.random.uniform(low=-0.055, high=0.055) * T
+        if (0 <= xNew and xNew <= 100):
+            yNew = aimFunction(xNew)
+            if yNew - y < 0:
+                x = xNew
+            else:
+                # metropolis principle
+                p = math.exp(-(yNew - y) / T)
+                r = np.random.uniform(low=0, high=1)
+                if r < p:
+                    x = xNew
+        t += 1
+        T = 1000 / (1 + t) #降温函数，也可使用 T=0.9T
+    print(x, aimFunction(x))
+    # 39.78060332087924 -32150.24487975278
